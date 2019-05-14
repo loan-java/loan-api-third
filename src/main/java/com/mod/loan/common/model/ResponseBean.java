0f57f81ca29bc13bd30be50005332772ba68eb24
LJJ <liujianjian@booksroo.com>
2019-05-14 21:56:33 +0800

    响应 bean

--- src/main/java/com/mod/loan/common/model/ResponseBean.java
+++ src/main/java/com/mod/loan/common/model/ResponseBean.java
@@ -3,15 +3,36 @@ package com.mod.loan.common.model;
 
 public class ResponseBean<T> {
 
-    private String code;
+    private int code;
     private String msg;
     private T data;
 
-    public String getCode() {
+    public ResponseBean() {
+    }
+
+    public ResponseBean(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static <T> ResponseBean<T> success(T data) {
+        return new ResponseBean<>(200, "成功", data);
+    }
+
+    public static <T> ResponseBean<T> fail(String msg) {
+        return new ResponseBean<>(-1, msg, null);
+    }
+
+    public static <T> ResponseBean<T> fail(String msg, T data) {
+        return new ResponseBean<>(-1, msg, data);
+    }
+
+    public int getCode() {
         return code;
     }
 
-    public void setCode(String code) {
+    public void setCode(int code) {
         this.code = code;
     }
 
