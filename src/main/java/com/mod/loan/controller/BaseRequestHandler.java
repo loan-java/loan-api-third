60941122c72303c007235ac11419fb8978fccb91
rchenzl <czl931122>
2019-05-17 17:38:37 +0800

    bank

--- src/main/java/com/mod/loan/controller/BaseRequestHandler.java
+++ src/main/java/com/mod/loan/controller/BaseRequestHandler.java
@@ -1,15 +1,9 @@
 package com.mod.loan.controller;
 
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.mod.loan.common.enums.ResponseEnum;
 import com.mod.loan.common.exception.BizException;
 import org.apache.commons.lang.StringUtils;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class BaseRequestHandler {
 
