1b7801d1e1e572f879dd5db5964806e9411749e1
LJJ <liujianjian@booksroo.com>
2019-05-19 14:53:38 +0800

    调整下sql脚本执行顺序

--- sql/update_1.0.0.sql
+++ sql/update_1.0.0.sql
@@ -2,11 +2,12 @@
 -- 修改订单号长度
 alter table tb_order change column order_no order_no varchar(40) not null comment '订单号';
 
--- 添加唯一索引
-create unique index uk_order_no on tb_order(order_no,source);
-
 -- 订单表添加来源
 alter table tb_order add column source int default 0 comment '订单来源：0-聚合，1-融泽';
+
+-- 添加唯一索引
+create unique index uk_order_source on tb_order(order_no,source);
+
 
 CREATE TABLE `tb_user_order` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
