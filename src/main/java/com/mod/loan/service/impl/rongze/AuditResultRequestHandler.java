140b4ebd1d8a3eedd62be8cf5dfaa1fe751d644a
whw <weihanwen1991@gmail.com>
2019-05-21 18:23:11 +0800

    ==

--- src/main/java/com/mod/loan/service/impl/rongze/AuditResultRequestHandler.java
+++ src/main/java/com/mod/loan/service/impl/rongze/AuditResultRequestHandler.java
@@ -168,7 +168,7 @@ public class AuditResultRequestHandler {
 
         int conclusion = 40; //10=审批通过 40=审批拒绝30=审批处理中
         String reapply = "1"; //是否可再申请 1-是，0-不可以
-        Timestamp approvalTime = new Timestamp(System.currentTimeMillis()); //是否可再申请 1-是，0-不可以
+        Timestamp approvalTime = new Timestamp(System.currentTimeMillis());
         String reapplyTime = DateFormatUtils.format(new Date().getTime() + (1000L * 3600 * 24 * 7), "yyyy-MM-dd"); //可再申请的时间，yyyy- MM-dd，比如（2020-10- 10）
         String remark = "审批拒绝";
         String creditDeadline = DateUtil.getStringDateShort(); //审批结果有效期，当前时间
@@ -177,7 +177,7 @@ public class AuditResultRequestHandler {
                 && userIdent.getLiveness() == 2 && userIdent.getBindbank() == 2) {
             conclusion = 10;
         }
-        map.put("reapplytime", approvalTime);
+        map.put("reapplytime", reapplyTime);
         map.put("pro_type", proType);
         map.put("term_unit", termUnit);
         map.put("amount_type", amountType);
