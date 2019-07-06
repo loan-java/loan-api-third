98c19a36acb516fe1b52745cebd4d52d3732ff9b
DESKTOP-M8L1VO6\admin <weihanwen1991@gmail.com>
2019-07-06 16:57:27 +0800

    探针A代码提交-判断出错修复

--- src/main/java/com/mod/loan/service/impl/TypeFilterServiceImpl.java
+++ src/main/java/com/mod/loan/service/impl/TypeFilterServiceImpl.java
@@ -188,7 +188,7 @@ public class TypeFilterServiceImpl implements TypeFilterService {
                     log.error("指针A=====9返回数据异常。" + postString);
                     return false;
                 }
-                if ("U".equalsIgnoreCase(resultCode) || "A".equalsIgnoreCase(resultCode)) {
+                if ("4".equals(resultCode) || "1".equals(resultCode)) {
                     return true;
                 }
             }
